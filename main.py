110b7614729739fd4d355571127831ee826e523f
ne_ilyxa <72783330+git1worker@users.noreply.github.com>
2022-02-18 18:58:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ list_warnings = []
 messages = {}
 
 
-def member_messenger(id):
+def member_messenger(id):  # Подсчет сообщений
     global messages
     if id in messages:
         messages[id] = messages[id] + 1
@@ -20,7 +20,7 @@ def member_messenger(id):
         messages[id] = 1
 
 
-async def antispam(k):
+async def antispam(k):  # Вызов предупреждения по спаму
     cursor.execute(f"""SELECT * FROM dis_users WHERE id_discord = {k}""")
     num = cursor.fetchone()[4]
     cursor.execute(f"""UPDATE dis_users SET warnings = {num + 1} WHERE id_discord = {k}""")
@@ -36,7 +36,7 @@ async def antispam(k):
     conn.commit()
 
 
-async def timer_messages(num_messages_in_5_sec):
+async def timer_messages(num_messages_in_3_sec):
     global flag
     global messages
     global list_warnings
@@ -46,9 +46,9 @@ async def timer_messages(num_messages_in_5_sec):
 
     for k, v in messages.items():
 
-        if v > num_messages_in_5_sec:  # Количество сообщений за 5 секунд # Антиспам!
-            await antispam(k)
-        cursor.execute(f"""UPDATE dis_users SET time_afk = 0 WHERE id_discord = {k}""")
+        if v > num_messages_in_3_sec:  # Количество сообщений за 3 секунды # Антиспам!
+            await antispam(k)  # Вызов предупреждения по спаму
+        cursor.execute(f"""UPDATE dis_users SET time_afk = 0 WHERE id_discord = {k}""")  # Обнуление счетчика афк
         cursor.execute(f"""SELECT * FROM dis_users WHERE id_discord = {k}""")
         num = cursor.fetchone()[2]
         cursor.execute(f"""UPDATE dis_users SET num_mess = {num + v} WHERE id_discord = {k}""")
@@ -65,12 +65,14 @@ async def timer_min():
         for member in members:
             if member.bot:
                 continue
-            if member.voice is not None and member.voice.channel != afk_voice:
+            if member.voice is not None and member.voice.channel != afk_voice:  # Если участник в войсе и не в афк
 
                 cursor.execute(f"""SELECT * FROM dis_users WHERE id_discord = {member.id}""")
                 num = cursor.fetchone()[3]
-                cursor.execute(f"""UPDATE dis_users SET time_on_voice = {num + 1} WHERE id_discord = {member.id}""")  # Счет времени в войсе
-                cursor.execute(f"""UPDATE dis_users SET time_afk = 0 WHERE id_discord = {member.id}""")  # Обнуление счетчика афк
+                cursor.execute(f"""UPDATE dis_users SET time_on_voice 
+                = {num + 1} WHERE id_discord = {member.id}""")  # Счет времени в войсе
+                cursor.execute(f"""UPDATE dis_users SET time_afk = 
+                0 WHERE id_discord = {member.id}""")  # Обнуление счетчика афк
                 conn.commit()
             cursor.execute(f"""SELECT * FROM dis_users WHERE id_discord = {member.id}""")
             num = cursor.fetchone()[5]
@@ -96,16 +98,17 @@ async def on_ready():
     global afk_voice
     global dead
 
-    text_chann = client.get_channel(938411811173199955)  # текстовый канал с которым взаимодействует bot
-    guild = client.get_guild(938411811173199952)  # айди сервера
-    new_role = guild.get_role(941606299144171591)  # роль новичка
+    text_chann = client.get_channel(938411811173199955)  # текстовый канал куда пишет bot
+    guild = client.get_guild(938411811173199952)  # Объект сервера
+    new_role = guild.get_role(941606299144171591)  # Роль для новичка
     afk_voice = guild.get_channel(944144703148937216)  # Объект афк канала
-    dead = guild.get_role(944202377500717126)  # Труп
-    conn = sqlite3.connect("./mydatabase.db")
+    dead = guild.get_role(944202377500717126)  # Роль Труп
+
+    conn = sqlite3.connect("./mydatabase.db") # Соединение с бд
     cursor = conn.cursor()
-    cursor.execute(f"""UPDATE dis_users SET warnings = 0 WHERE id_discord = 869217405862305822""")
+    cursor.execute(f"""UPDATE dis_users SET warnings = 0 WHERE id_discord = 869217405862305822""")  # Обнуление предупреждений
     conn.commit()
-    await timer_min()
+    await timer_min()  # Обновление каждую минуту время в войсе и афк
 
 
 @client.event
@@ -131,19 +134,20 @@ async def on_message(message):
                 await antispam(message.author.id)
                 await message.delete()
             else:
-                await timer_messages(3)  # Если меньше 200 символов за 3 секунд ограничение - 4 сообщений
+                await timer_messages(3)  # Если меньше 400 символов за 3 секунд ограничение - 4 сообщений
     elif message.channel.id == 944209923645001728:  # Канал для комманд боту
         await client.process_commands(message)
 
+    """КОМАНДЫ"""
 
 @client.command()
-async def help_me(ctx):
+async def help_me(ctx):  # Команда help_me Выводит доступные команды
     await ctx.send("$get_info_voice id  - Сколько участник сидел в голосовом чате (id человека)\n"
                    "\n$get_info_messages id  - Сколько участник писал в текстовом чате (id человека)\n"
                    "\n$get_info_warnings id  - Сколько у участника предупреждений (id человека)")
 
 @client.command()
-async def get_info_voice(ctx, arg):
+async def get_info_voice(ctx, arg):  # Команда get_info_voice Выводит время в голосовых чатах
     global cursor
     try:
         cursor.execute(f"""SELECT * FROM dis_users WHERE id_discord = {arg}""")
@@ -160,7 +164,7 @@ async def get_info_voice(ctx, arg):
         await ctx.send("Произошла ошибка")
 
 @client.command()
-async def get_info_messages(ctx, arg):
+async def get_info_messages(ctx, arg):  # Команда get_info_messages Выводит число сообщений за все время
     global cursor
     try:
         cursor.execute(f"""SELECT * FROM dis_users WHERE id_discord = {arg}""")
@@ -172,7 +176,7 @@ async def get_info_messages(ctx, arg):
         await ctx.send("Произошла ошибка")
 
 @client.command()
-async def get_info_warnings(ctx, arg):
+async def get_info_warnings(ctx, arg):  # Команда get_info_warnings Выводит число предупреждений
     global cursor
     try:
         cursor.execute(f"""SELECT * FROM dis_users WHERE id_discord = {arg}""")
