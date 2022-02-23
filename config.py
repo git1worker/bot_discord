9d32000ecc29698fc34925c7e72c19e67bb73fe9
git1worker <vladim-b@mail.ru>
2022-02-23 18:48:25 +0300

    alpha 0.3

---
+++ config.py
@@ -0,0 +1,15 @@
+
+token = "OTM4NDEyMjM1MjcxODU2MTc5.Yfp6fw.X2GqV6kPo7_Z4wq_goSEfWxFmjo"
+# import time
+# last = time.time()
+#
+#
+# while time.time() - last < 5:
+#     print(1)
+#
+# list = []
+#
+# def spl(list):
+#     return [n for n in str(list)]
+#
+# print(spl(list))
